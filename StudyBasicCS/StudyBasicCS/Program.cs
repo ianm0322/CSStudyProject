03dff918e3fdc386d19e58dbc9df42a64bbab36d
ianm0322 <46345017+ianm0322@users.noreply.github.com>
2023-03-24 09:01:22 +0900

    C# 기초반 학습용 프로젝트
    C# 기초반 학습용 프로젝트

---
+++ StudyBasicCS/StudyBasicCS/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StudyBasicCS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
